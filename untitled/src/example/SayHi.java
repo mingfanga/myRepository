7c2d58a3b237a960c9fc14daae72ee5805391b77
mingfanga <mingfanga@isoftstone.com>
2017-12-13 17:19:17 +0800

    提交新功能

--- untitled/src/example/SayHi.java
+++ untitled/src/example/SayHi.java
@@ -4,6 +4,7 @@ public class SayHi {
     public String sayHi(String from) {
         String result = "Hi, world, from " + from;
         System.out.println(result);
+        System.out.println("2131");
         return result;
     }
 }
