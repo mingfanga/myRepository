85d7893fb169bc01093456f8917faca714589c56
mingfanga <mingfanga@isoftstone.com>
2017-12-14 15:22:03 +0800

    add 123

--- untitled/src/example/SayHi.java
+++ untitled/src/example/SayHi.java
@@ -5,6 +5,7 @@ public class SayHi {
         String result = "Hi, world, from " + from;
         System.out.println(result);
         System.out.println("I'm trying to deal it.");
+        System.out.println("add 123.");
         return result;
     }
 }
